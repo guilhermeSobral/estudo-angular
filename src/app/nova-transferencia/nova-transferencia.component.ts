7c9e9b0e307d0708c522d385475363d2decb11f7
Guilherme Sobral de Barros <guilhermesbarros13@gmail.com>
2021-05-24 21:21:17 -0300

    Modulo 1 Completo

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { Transferencia } from "../models/transferencia.model";
+import { TransferenciaService } from "../services/transferencia.service";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -6,5 +9,34 @@ import { Component } from "@angular/core";
     styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
+
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: number;
+
+    constructor(private service: TransferenciaService, private router: Router) {}
+
+    transferir() {
+       console.log('Solicitada nova transferencia');
+       const valorEmitir: Transferencia = {
+        valor: this.valor,
+        destino: this.destino
+       };
+       this.service.adicionar(valorEmitir)
+        .subscribe(resultado => {
+           console.log(resultado);
+           this.limparCampos();
+           this.router.navigateByUrl('extrato');
+       }, error => {
+           console.error(error);
+       });
+       
+    }
+
+    limparCampos() {
+        this.valor = 0;
+        this.destino = 0;
+    }
 
 }
